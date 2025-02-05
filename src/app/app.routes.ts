2c0487f66fa611b17f384a744e05b4e5fcad46d4
luccahonorio <luccahmaraujo1@gmail.com>
2025-02-05 17:26:01 -0300

    feat: create the home page with header and adding the angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
